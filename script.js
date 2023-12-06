c3b2a76bbc22c425a93e1d8cb533cf7dd45c18e5
bileeave <jubileegavenido@gmail.com>
2023-12-06 10:53:42 +0800

    Add buttons for user selection

--- script.js
+++ script.js
@@ -5,28 +5,28 @@ function getComputerChoice() {
     const choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random() * choice.length)];
 }
-function playRound(playerSelection, computerSelection) {
-    let selection = playerSelection.toLowerCase();
-    if (selection === computerSelection) {
-        return "Ooh, it's a tie!";
-    } else if (selection === "rock" && computerSelection === "paper") {
-        return "You lose! Paper beats rock.";
-    } else if (selection === "rock" && computerSelection === "scissors") {
-        return "You win! Rock beats scissor.";
-    } else if (selection === "paper" && computerSelection === "scissors") {
-        return "You lose! Scissors beats paper.";
-    } else if (selection === "paper" && computerSelection === "rock") {
-        return "You win! Paper beats rock.";
-    } else if (selection === "scissors" && computerSelection === "rock") {
-        return "You lose! Rock beats scissors.";
-    } else if (selection === "scissors" && computerSelection === "paper") {
-        return "You win! Scissors beats paper."
-    } else if (selection === "scissor") {
-        return "I cannot accept the word \"scissor\" yet. Sorry, please add 's'.";
-    } else {
-        return "Please type any of these choices: rock, paper, or scissors.";
-    }
-}
+// function playRound(playerSelection, computerSelection) {
+//     let selection = playerSelection.toLowerCase();
+//     if (selection === computerSelection) {
+//         return "Ooh, it's a tie!";
+//     } else if (selection === "rock" && computerSelection === "paper") {
+//         return "You lose! Paper beats rock.";
+//     } else if (selection === "rock" && computerSelection === "scissors") {
+//         return "You win! Rock beats scissor.";
+//     } else if (selection === "paper" && computerSelection === "scissors") {
+//         return "You lose! Scissors beats paper.";
+//     } else if (selection === "paper" && computerSelection === "rock") {
+//         return "You win! Paper beats rock.";
+//     } else if (selection === "scissors" && computerSelection === "rock") {
+//         return "You lose! Rock beats scissors.";
+//     } else if (selection === "scissors" && computerSelection === "paper") {
+//         return "You win! Scissors beats paper."
+//     } else if (selection === "scissor") {
+//         return "I cannot accept the word \"scissor\" yet. Sorry, please add 's'.";
+//     } else {
+//         return "Please type any of these choices: rock, paper, or scissors.";
+//     }
+// }
 function game() {
     let playerSelection = prompt("Should you pick rock, paper, or scissors?");
     for (let i = 0; i < 5; i++) {
