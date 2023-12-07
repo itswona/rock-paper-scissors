fbacdd15e6d6aa39f4ac395158c8e89ea8a17193
bileeave <jubileegavenido@gmail.com>
2023-12-07 09:00:21 +0800

    Refactor code

--- script.js
+++ script.js
@@ -4,27 +4,24 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
-// let humanPoints = "";
-// let computerPoints = 0;
-
 function playRound(playerSelection, computerSelection) {
     let selection = playerSelection.toLowerCase();
     if (selection === computerSelection) {
-        return "Ooh, it's a tie!";
+        result.textContent = "Ooh, it's a tie!";
     } else if (selection === "rock" && computerSelection === "paper") {
-        return "You lose! Paper beats rock.";
+        result.textContent = "You lose! Paper beats rock.";
     } else if (selection === "rock" && computerSelection === "scissors") {
-        return "You win! Rock beats scissor.";
+        result.textContent = "You win! Rock beats scissor.";
     } else if (selection === "paper" && computerSelection === "scissors") {
-        return "You lose! Scissors beats paper.";
+        result.textContent = "You lose! Scissors beats paper.";
     } else if (selection === "paper" && computerSelection === "rock") {
-        return "You win! Paper beats rock.";
+        result.textContent = "You win! Paper beats rock.";
     } else if (selection === "scissors" && computerSelection === "rock") {
-        return "You lose! Rock beats scissors.";
+        result.textContent = "You lose! Rock beats scissors.";
     } else if (selection === "scissors" && computerSelection === "paper") {
-        return "You win! Scissors beats paper."
+        result.textContent = "You win! Scissors beats paper."
     } else {
-        return "Please type any of these choices: rock, paper, or scissors.";
+        result.textContent = "Please type any of these choices: rock, paper, or scissors.";
     }
 }
 
@@ -37,31 +34,28 @@ const btnRock = document.querySelector("#rock");
 const btnPaper = document.querySelector("#paper");
 const btnScissors = document.querySelector("#scissors");
 
-const humanScore = document.querySelector("#human");
-const computerScore = document.querySelector("#computer");
+const humanScore = document.querySelector("#humanScore");
+const computerScore = document.querySelector("#computerScore");
 const result = document.querySelector("#result");
+
+humanScore.textContent = "";
+computerScore.textContent = "";
 
 btnRock.textContent = "Rock";
 btnPaper.textContent = "Paper";
 btnScissors.textContent = "Scissors";
 
-humanScore.textContent = humanPoints;
-computerScore.textContent = computerPoints;
-
 selection.addEventListener("click", (event) => {
     let target = event.target;
     switch (target.id) {
         case "rock":
-            result.textContent = game(btnRock.textContent);
+            game(btnRock.textContent);
             break;
         case "paper":
-            result.textContent = game(btnPaper.textContent);
+            game(btnPaper.textContent);
             break;
         case "scissors":
-            result.textContent = game(btnScissors.textContent);
-            break;
-        default:
-            result.textContent = "Undefined value";
+            game(btnScissors.textContent);
             break;
     }
 });
